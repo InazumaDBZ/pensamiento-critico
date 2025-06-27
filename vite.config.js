3dfd5789dda63ace2ac5ca787618e25ca2921f3c
Eliuth Cordero <bryan.e.cordero@gmail.com>
2025-06-26 22:04:40 -0600

    estilos

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'node:path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import tailwindcss from '@tailwindcss/vite';  
 
-// https://vitejs.dev/config/
 export default defineConfig({
   // 🏠 Ruta raíz para GH Pages
   base: '/pensamiento-critico/',
-  plugins: [react()],
+  plugins: [
+    react(),        // plugin de React
+    tailwindcss(),  // plugin de Tailwind  ← añadido
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
